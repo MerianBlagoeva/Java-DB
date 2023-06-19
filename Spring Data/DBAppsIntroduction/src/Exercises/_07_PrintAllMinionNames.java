6693e40ecbac06716b3448f601c62a5a88fae42a
Merian <drundrun00@gmail.com>
2023-06-19 20:46:47 +0300

    Made more methods

--- Spring Data/DBAppsIntroduction/src/Exercises/_07_PrintAllMinionNames.java
+++ Spring Data/DBAppsIntroduction/src/Exercises/_07_PrintAllMinionNames.java
@@ -9,10 +9,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _07_PrintAllMinionNames {
+    private static Connection connection;
 
     public static void main(String[] args) throws SQLException, IOException {
-        Connection connection = Utils.getConnection();
+        connection = Utils.getConnection();
 
+        List<String> minionNames = getMinionNames();
+
+        printMinionNames(minionNames);
+    }
+
+    private static void printMinionNames(List<String> minionNames) {
+        for (int i = 0; i < minionNames.size() / 2; i++) {
+            System.out.println(minionNames.get(i));
+            System.out.println(minionNames.get(minionNames.size() - 1 - i));
+        }
+    }
+
+    private static List<String> getMinionNames() throws SQLException {
         PreparedStatement preparedStatement = connection
                 .prepareStatement("SELECT name FROM minions;");
 
@@ -24,9 +38,6 @@ public class _07_PrintAllMinionNames {
             minionNames.add(resultSet.getString("name"));
         }
 
-        for (int i = 0; i < minionNames.size() / 2; i++) {
-            System.out.println(minionNames.get(i));
-            System.out.println(minionNames.get(minionNames.size() - 1 - i));
-        }
+        return minionNames;
     }
 }
